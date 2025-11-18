bdf0f297c761da48801ec8535e31f8e198238e3c
BotsheloSe <st10445866@imconnect.edu.za>
2025-11-18 13:28:13 +0200

    Final push of the Unit Tests Project File

---
+++ xUnit Test/SimpleTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace xUnit_Test
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void Test1_OnePlusOneEqualsTwo()
+        {
+            // Arrange
+            int a = 1;
+            int b = 1;
+
+            // Act
+            int result = a + b;
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test2_StringContains()
+        {
+            // Arrange
+            string testString = "Hello World";
+
+            // Act & Assert
+            Assert.Contains("Hello", testString);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Test3_TheoryExample(int number)
+        {
+            // This tests multiple inputs
+            Assert.True(number > 0);
+        }
+    }
+}
